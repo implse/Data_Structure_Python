7455be5574a99f653271707dbecf79c7de45a5ab
finite <finite.impulse@gmail.com>
2018-01-23 01:50:16 +0100

    Fix Stack

--- Linked_List/Linked_List.py
+++ Linked_List/Linked_List.py
@@ -1,7 +1,7 @@
 class Node(object):
 	def __init__(self, value):
 		self.value = value
-		self.next_node = None
+		self.next = None
 
 class LinkedList(object):
 	def __init__(self):
@@ -13,7 +13,7 @@ class LinkedList(object):
 	def add_to_head(self, value):
 		new_node = Node(value)
 		if self.head:
-			new_node.next_node = self.head
+			new_node.next = self.head
 			self.head = new_node
 		else:
 			self.head = new_node
@@ -23,7 +23,7 @@ class LinkedList(object):
 	def add_to_tail(self, value):
 		new_node = Node(value)
 		if self.tail:
-			self.tail.next_node = new_node
+			self.tail.next = new_node
 			self.tail = new_node
 		else:
 			self.head = new_node
@@ -33,63 +33,63 @@ class LinkedList(object):
 	# Add value at specific index in list
 	def add_at(self, value, index):
 		new_node = Node(value)
-		previous_node = None
-		current_node = self.head
+		previous = None
+		current = self.head
 		i = 0
-		while i < index and current_node.next_node:
-			previous_node = current_node
-			current_node = current_node.next_node
+		while i < index and current.next:
+			previous = current
+			current = current.next
 			i += 1
 		if i == index:
-			previous_node.next_node = new_node
-			new_node.next_node = current_node
+			previous.next = new_node
+			new_node.next = current
 			return True
 		else:
 			return False
 
 	# Remove value: return true or false
 	def remove(self, value):
-		previous_node = None
-		current_node = self.head
-		while current_node:
-			if current_node.value == value:
-				if previous_node:
-					previous_node.next_node = current_node.next_node
+		previous = None
+		current = self.head
+		while current:
+			if current.value == value:
+				if previous:
+					previous.next = current.next
 				else:
-					self.head = current_node.next_node
+					self.head = current.next
 				self.size -= 1
 				return True
-			previous_node = current_node
-			current_node = current_node.next_node
+			previous = current
+			current = current.next
 		return False
 
 	# Find if value is in list: return true or false
 	def find(self, value):
-		current_node = self.head
-		while current_node:
-			if current_node.value == value:
+		current = self.head
+		while current:
+			if current.value == value:
 				return True
-			current_node = current_node.next_node
+			current = current.next
 		return False
 
 	# Reverse the link list
 	def reverse(self):
-		current_node = self.head
-		next_node = None
-		previous_node = None
-		while current_node:
-			next_node = current_node.next_node
-			current_node.next_node = previous_node
-			previous_node = current_node
-			current_node = next_node
-			self.head = previous_node
+		current = self.head
+		next = None
+		previous = None
+		while current:
+			next = current.next
+			current.next = previous
+			previous = current
+			current = next
+			self.head = previous
 		return self.head
 
 	# Create list from linkedList
 	def to_list(self):
 		l = []
-		current_node = self.head
-		while current_node:
-			l.append(current_node.value)
-			current_node = current_node.next_node
+		current = self.head
+		while current:
+			l.append(current.value)
+			current = current.next
 		return l
--- Stack/Stack.py
+++ Stack/Stack.py
@@ -1,17 +1,17 @@
 class Node(object):
-	def __init__(self, value):
-		self.value = value
-		self.next_node = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 class Stack(object):
-    def __init__(self, value):
+    def __init__(self):
         self.top = None
         self.size = 0
 
     def push(self, value):
         new_node = Node(value)
-        if self.top:
-            new_node.next_node = self.top
+        if self.top != None:
+            new_node.next = self.top
         self.top = new_node
         self.size += 1
 
@@ -19,6 +19,6 @@ class Stack(object):
         if self.top is None:
             return None
         pop_value = self.top.value
-        self.top = self.top.next_node
+        self.top = self.top.next
         self.size -= 1
         return pop_value
