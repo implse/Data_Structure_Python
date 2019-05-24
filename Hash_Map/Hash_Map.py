3c869677117678eeb12fb8fe18afee18f602edcd
Finite <finite.impulse@gmail.com>
2019-05-24 17:05:59 +0200

    Hash Map - Seperate Chaining

--- Hash_Map/Hash_Map.py
+++ Hash_Map/Hash_Map.py
@@ -1,105 +1,54 @@
-class HashMap:
-    def __init__(self, array_size):
-        self.array_size = array_size
-        self.array = [None for item in range(array_size)]
-    # Hash Function
-    def hash(self, key, count_collisions=0):
-        key_bytes = key.encode()
-        hash_code = sum(key_bytes)
-        return hash_code + count_collisions
+from Linked_List import LinkedList, Node
 
-    def compressor(self, hash_code):
+# Hash Map - Seperate Chaining (Linked List)
+class HashMap:
+    def __init__(self, size):
+        self.array_size = size
+        self.array = [LinkedList() for _ in range(self.array_size)]
+
+    # Hash Function
+    def hash(self, key):
+        return sum(key.encode())
+
+    def compress(self, hash_code):
         return hash_code % self.array_size
 
-    # Set key, value
+    # Set Key / Value
     def set(self, key, value):
-        array_index = self.compressor(self.hash(key))
-        current_array_value = self.array[array_index]
+        array_index = self.compress(self.hash(key))
+        node = Node([key, value])
+        list_at_array = self.array[array_index]
+        for item in list_at_array:
+            if item[0] == key:
+                item[1] = value
+                return
+        list_at_array.insert(node)
 
-        if current_array_value is None:
-            self.array[array_index] = [key, value]
-            return
-
-        if current_array_value[0] == key:
-            self.array[array_index] = [key, value]
-            return
-
-        # Collision
-        number_collisions = 1
-
-        while(current_array_value[0] != key):
-          new_hash_code = self.hash(key, number_collisions)
-          new_array_index = self.compressor(new_hash_code)
-          current_array_value = self.array[new_array_index]
-
-          if current_array_value is None:
-              self.array[new_array_index] = [key, value]
-              return
-
-          if current_array_value[0] == key:
-              self.array[new_array_index] = [key, value]
-              return
-
-          number_collisions += 1
-
-        return
-
-    # Get value from Key
+    # Get Value from Key
     def get(self, key):
-        array_index = self.compressor(self.hash(key))
-        possible_return_value = self.array[array_index]
+        array_index = self.compress(self.hash(key))
+        list_at_index = self.array[array_index]
+        for item in list_at_index:
+            if item[0] == key:
+                return item[1]
+        return None
 
-        if possible_return_value is None:
-            return None
-
-        if possible_return_value[0] == key:
-            return possible_return_value[1]
-
-        retrieval_collisions = 1
-
-        while (possible_return_value != key):
-            new_hash_code = self.hash(key, retrieval_collisions)
-            retrieving_array_index = self.compressor(new_hash_code)
-            possible_return_value = self.array[retrieving_array_index]
-
-            if possible_return_value is None:
-                return None
-
-            if possible_return_value[0] == key:
-                return possible_return_value[1]
-
-            retrieval_collisions += 1
-
-        return
-
-    # Return the Keys
+    # List all the Keys
     def keys(self):
-            for k in self.array:
-                if k != None:
-                    print(k[0])
-    # Return Key Value
+        for k in self.array:
+            current_node = k.head_node
+            if current_node != None:
+                while current_node:
+                    key = current_node.value[0]
+                    print(key)
+                    current_node = current_node.get_next_node()
+
+    # Return Key-Value Pair
     def items(self):
         for k in self.array:
-            if k != None:
-                print((k[0], k[1]))
-
-
-
-
-
-# Test
-
-hash_map = HashMap(20)
-hash_map.set("John Coltrane", "North Carolina")
-hash_map.set("Charlie Parker", "Missouri")
-hash_map.set("Charles Mingus", "Arizona")
-hash_map.set("Duke Ellington", "Ohio")
-hash_map.set("Dizzy Gillepsie", "South Carolina")
-hash_map.set("Thelonious Monk", "South Carolina")
-hash_map.set("Bill Evans", "New Jersey")
-
-
-print(hash_map.get("John Coltrane"))
-
-hash_map.keys()
-hash_map.items()
+            current_node = k.head_node
+            if current_node != None:
+                while current_node:
+                    key, value = current_node.value
+                    print((key, value))
+                    current_node = current_node.get_next_node()
