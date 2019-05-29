864a2eb7ba879fb8028357ab427852e8c3b59b3f
Finite <finite.impulse@gmail.com>
2019-05-29 15:52:41 +0200

    Add Min Heap

--- Heap/minHeap.py
+++ Heap/minHeap.py
@@ -1,10 +1,3 @@
-# Min Heap Construction
-
-# Implement a Min Heap class. The class should support insertion, removal (of the minimum / root value),
-# peeking (of the minimum / root value), as well as sifting a value up and down the
-# heap and building the heap from an input array. The heap should be represented in the form of an array.
-
-
 class MinHeap:
     def __init__(self, array):
         self.heap = self.heapify(array)
